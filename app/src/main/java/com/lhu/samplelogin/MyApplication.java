9a1b961d44b43f20373c30e3fc8534993227b18c
hyung-uk <lhu114@nate.com>
2015-10-21 13:48:11 +0900

    Initial commit

---
+++ app/src/main/java/com/lhu/samplelogin/MyApplication.java
@@ -0,0 +1,22 @@
+package com.lhu.samplelogin;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by Tacademy on 2015-10-21.
+ */
+public class MyApplication extends Application {
+    private static Context mContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mContext = this;
+    }
+
+    public static Context getContext() {
+        return mContext;
+    }
+}
---
+++ app/src/main/java/com/lhu/samplelogin/NetworkManager.java
@@ -0,0 +1,44 @@
+package com.lhu.samplelogin;
+
+import android.os.Handler;
+import android.os.Looper;
+
+/**
+ * Created by Tacademy on 2015-10-21.
+ */
+public class NetworkManager {
+    private static NetworkManager instance;
+    public static NetworkManager getInstance() {
+        if (instance == null) {
+            instance = new NetworkManager();
+        }
+        return instance;
+    }
+    private NetworkManager() {
+
+    }
+
+    public interface OnResultListener<T> {
+        public void onSuccess(T result);
+        public void onFail(int code);
+    }
+
+    Handler mHadler = new Handler(Looper.getMainLooper());
+    public void login(String userid, String password, final OnResultListener<String> listener) {
+        mHadler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                listener.onSuccess("ok");
+            }
+        }, 1000);
+    }
+
+    public void signup(String userid, String password, final OnResultListener<String> listener) {
+        mHadler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                listener.onSuccess("ok");
+            }
+        }, 1000);
+    }
+}
